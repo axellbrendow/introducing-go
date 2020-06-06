e99a43fbcfed4afe9e0c30534f80d91d3d6f4b47
axell-brendow <breno.axel@gmail.com>
2020-06-06 00:05:49 -0300

    Add methods creation

---
+++ chapter7-structs-interfaces/structs3-methods.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	x, y, r float64
+}
+
+// You can use dot syntax to access Circle and *Circle
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
+
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
+
+func main() {
+	c := Circle{0, 0, 5}
+	fmt.Println(c.area())
+
+	r := Rectangle{0, 0, 10, 10}
+	fmt.Println(r.area())
+}
