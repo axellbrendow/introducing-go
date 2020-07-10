ef0f52c411df3cb75613f9932aeba467530f2a22
axell-brendow <breno.axel@gmail.com>
2020-07-10 18:28:05 -0300

    Upload example of how to read a file with io/ioutil

---
+++ chapter8-packages/files2-test-with-ioutil.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	// read the file
+	fileBytes, err := ioutil.ReadFile("test.txt")
+	if err != nil {
+		fmt.Println("Could not read file data")
+		return
+	}
+
+	str := string(fileBytes)
+	fmt.Println(str)
+}
