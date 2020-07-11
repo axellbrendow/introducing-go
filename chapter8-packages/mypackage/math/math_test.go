3152d6298ff1fbf5b29589493aec30c59a0e406e
axell-brendow <breno.axel@gmail.com>
2020-07-11 13:26:51 -0300

    Add package test example
    To run the test go to mypackage/math folder and execute `go test`

---
+++ chapter8-packages/mypackage/math/math_test.go
@@ -0,0 +1,35 @@
+package math
+
+import (
+	"fmt"
+	"testing"
+)
+
+func init() {
+	fmt.Println("mypackage/math package test initialized")
+}
+
+type testpair struct {
+	values  []float64
+	average float64
+}
+
+var tests = []testpair{
+	{[]float64{1, 2}, 1.5},
+	{[]float64{1, 1, 1, 1, 1, 1}, 1},
+	{[]float64{-1, 1}, 0},
+}
+
+// TestAverage is an exported function because it starts with a capital letter
+func TestAverage(t *testing.T) {
+	for _, pair := range tests {
+		v := Average(pair.values)
+		if v != pair.average {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.average,
+				"got", v,
+			)
+		}
+	}
+}
